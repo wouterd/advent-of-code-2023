d7e590552ff619ff06ac82552490f1615c00f6b0
Wouter Danes <wouter.danes@gmail.com>
2023-12-09 12:20:48 +0100

    Day 9, Part 2

---
+++ day9/part-2.py
@@ -0,0 +1,29 @@
+import fileinput
+import os
+
+inputFile = os.path.dirname(__file__) + os.path.sep + 'input'
+
+sum = 0
+for l in fileinput.input(inputFile):
+    line = l.strip()
+    values = list(map(lambda x: int(x), line.split()))
+    deltas = []
+    curr = values
+    while True:
+        curr_deltas = []
+        for i in range(1,len(curr)):
+            curr_deltas.append(curr[i] - curr[i-1])
+        deltas.append(curr_deltas)
+        non_zero = next((x for x in curr_deltas if x != 0), None)
+        if not non_zero: break
+        curr = curr_deltas
+
+    decrease = 0
+    for _ in range(0,len(deltas)):
+        decrease = deltas.pop()[0] - decrease
+
+    next_value = values[0] - decrease
+    print(next_value)
+    sum += next_value
+
+print(sum)
