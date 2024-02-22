f4278729d3267d045e3170e6f1dce865056172bd
Wouter Danes <wouter.danes@postnl.nl>
2024-02-23 00:20:50 +0100

    Some python formatting

--- day9/part-1.py
+++ day9/part-1.py
@@ -4,22 +4,23 @@ import os
 inputFile = os.path.dirname(__file__) + os.path.sep + 'input'
 
 sum = 0
-for l in fileinput.input(inputFile):
-    line = l.strip()
+for ln in fileinput.input(inputFile):
+    line = ln.strip()
     values = list(map(lambda x: int(x), line.split()))
     deltas = []
     curr = values
     while True:
         curr_deltas = []
-        for i in range(1,len(curr)):
+        for i in range(1, len(curr)):
             curr_deltas.append(curr[i] - curr[i-1])
         deltas.append(curr_deltas)
         non_zero = next((x for x in curr_deltas if x != 0), None)
-        if not non_zero: break
+        if not non_zero:
+            break
         curr = curr_deltas
 
     increment = 0
-    for _ in range(0,len(deltas)):
+    for _ in range(0, len(deltas)):
         increment += deltas.pop().pop()
 
     next_value = values.pop() + increment
--- day9/part-2.py
+++ day9/part-2.py
@@ -4,25 +4,27 @@ import os
 inputFile = os.path.dirname(__file__) + os.path.sep + 'input'
 
 sum = 0
-for l in fileinput.input(inputFile):
-    line = l.strip()
+for ln in fileinput.input(inputFile):
+    line = ln.strip()
     values = list(map(lambda x: int(x), line.split()))
     deltas = []
     curr = values
     while True:
         curr_deltas = []
-        for i in range(1,len(curr)):
+        for i in range(1, len(curr)):
             curr_deltas.append(curr[i] - curr[i-1])
         deltas.append(curr_deltas)
         non_zero = next((x for x in curr_deltas if x != 0), None)
-        if not non_zero: break
+        if not non_zero:
+            break
         curr = curr_deltas
 
     decrease = 0
-    for _ in range(0,len(deltas)):
+    for _ in range(0, len(deltas)):
         decrease = deltas.pop()[0] - decrease
 
     next_value = values[0] - decrease
     sum += next_value
 
 print(sum)
+
